f3aa59a4ae5d00e655dc3071d25ab26138519cc1
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-04-12 18:52:52 +0530

    Catch exceptions

--- src/JAR.hs
+++ src/JAR.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module JAR where
 
 import Codec.Archive.Zip
@@ -6,9 +7,8 @@ import Control.Monad.Catch
 import Control.Monad.IO.Class (MonadIO(..))
 import Data.Map.Lazy (keys)
 import Path
-import Path.IO (copyFile)
 import Data.ByteString.Internal
-import System.Directory hiding (copyFile)
+
 
 getFilesFromJar :: (MonadThrow m, MonadCatch m, MonadIO m) =>
                    FilePath -> m [(Path Rel File, ByteString)]
@@ -20,14 +20,9 @@ getFilesFromJar jarLocation =
             contents <- getEntry es
             return (unEntrySelector es, contents)
 
-withUnsafePath :: (MonadThrow m) => FilePath -> (Path Rel File -> m [(Path Rel File, ByteString)]) -> (Path Abs File -> m [(Path Rel File, ByteString)]) -> m [(Path Rel File, ByteString)]
-withUnsafePath filepath action1 action2 =
-  catch (do
-            x <- parseRelFile filepath
-            y <- action1 x
-            return y)
-         (\_ -> do
-             x <- parseAbsFile filepath
-             y <- action2 x
-             return y)
+withUnsafePath :: (MonadCatch m, MonadThrow m) => FilePath -> (Path Rel File -> m [(Path Rel File, ByteString)]) -> (Path Abs File -> m [(Path Rel File, ByteString)]) -> m [(Path Rel File, ByteString)]
+withUnsafePath filepath action1 action2 = 
+  catch (parseRelFile filepath >>= action1) handler
+  where handler :: (MonadCatch m, MonadThrow m) => PathParseException -> m [(Path Rel File, ByteString)]
+        handler _ = undefined -- parseAbsFile filepath >>= action2
 
